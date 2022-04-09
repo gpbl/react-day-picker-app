e4b4a9520883f305c754741ce1a68661c59d9acc
Jonathan del Strother <jdelStrother@gmail.com>
2022-04-09 14:11:59 +0100

    Add react-day-picker 7.4.10 - 181,137 bytes

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,15 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import ReactDayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'))
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <ReactDayPicker />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
