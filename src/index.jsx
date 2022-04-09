c71b3fc072215a5fd7389626551bd70dae89d975
Jonathan del Strother <jdelStrother@gmail.com>
2022-04-09 14:18:36 +0100

    Update to react-day-picker 8.0.2 - 261,924 bytes

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import ReactDayPicker from "react-day-picker";
-import "react-day-picker/lib/style.css";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 const App = () => {
   return (
     <div>
       <h1>Hello, world!</h1>
-      <ReactDayPicker />
+      <DayPicker />
     </div>
   );
 };
